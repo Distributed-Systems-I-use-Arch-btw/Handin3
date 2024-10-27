fa116efa4be685e25bfbc760ed3c0d459c8f7102
Lucas <lucasveha@gmail.com>
2024-10-27 19:21:16 +0100

    Better arg handling

--- launch.go
+++ launch.go
@@ -10,12 +10,18 @@ import (
 func main() {
 	arg := os.Args
 
-	fmt.Println(arg[1])
+    if len(arg) < 2 {
+        fmt.Println("Please specify 'client' or 'server' as the only argument.")
+        os.Exit(1)
+    }
 
-	switch arg[1] {
-	case "client":
-		client.Run()
-	case "server":
-		server.Run()
-	}
+    switch arg[1] {
+    case "client":
+        client.Run()
+    case "server":
+        server.Run()
+    default:
+        fmt.Printf("Unknown argument: %s. Use 'client' or 'server'.\n", arg[1])
+        os.Exit(1)
+    }
 }
